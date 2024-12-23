ab55900f5a8fa6da0e062ea3bf0db5cacfaa1d53
Jagroop <jagroopsingh1936@gmail.com>
2024-12-22 21:47:49 -0800

    added sidebar, added buttons, fixed general styling, changed theme, added dropdown menu to navigation bar,

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 
 # meta tags for mobile use
 # MORPH, MINTY,
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MORPH],suppress_callback_exceptions=True,
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY],suppress_callback_exceptions=True,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
--- index.py
+++ index.py
@@ -19,20 +19,61 @@ from pages import choropleth1, scatterplot1, scatterplot2, choropleth2
 cardMap = dbc.Card([])
 
 navBar = dbc.NavbarSimple(
-    dbc.NavItem(dbc.NavLink("Table of Contents", href="/pages/scatterplot1")),
+    dbc.NavItem(dbc.DropdownMenu(label="Important Links", children=[
+        dbc.DropdownMenuItem("Donate Today!", href='https://ncadv.org/donate'),
+        dbc.DropdownMenuItem("LinkedIn", href='www.linkedin.com/in/jagroop-s-sam18731', external_link=True),
+    ],)),
     brand = 'Measuring the Socioeconomic Factors on Domestic Violence: A Global & National Case Study',
-    color='green',
+    # #A3E4D7
+    # #003366 - Navy Blue
+    # #2E856E - Clean
+    #
+    color='#2E856E',
     dark = True,
 )
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "22rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+sidebar =  html.Div(
+    [
+        html.H2("Table of Contents", className="display-2"),
+        html.Hr(),
+        dbc.Nav(
+            [
+                dbc.NavLink("Status of Laws", href="/pages/choropleth1", active="exact"),
+                dbc.NavLink("Possible Correlations", href="/pages/scatterplot1", active="exact"),
+                dbc.NavLink("3D-Analysis", href="/pages/scatterplot2", active="exact"),
+                dbc.NavLink("American Analysis", href="/pages/choropleth2", active="exact"),
+                dbc.NavLink("Correlations in American Data", href="/", active="exact"),
+                dbc.NavLink("Global Data Clustering & Segmentation", href="/", active="exact"),
+                dbc.NavLink("American Data Clustering & Segmentation", href="/2", active="exact"),
+                dbc.NavLink("Predictive Modeling", href="/", active="exact"),
+                dbc.NavLink("Conclusion", href="/", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style= SIDEBAR_STYLE,
+)
+
 
 
 # Landing Page
 app.layout = html.Div([
     navBar,
+    sidebar,
+
     # dash component to read url using pathname
     dcc.Location(id='url', refresh=False, pathname=''),
     html.Div([
-        dbc.Button("Next Page", href="/pages/choropleth1"),
         dcc.Link("Link to Map", href="/pages/choropleth1"),
         dcc.Link('Link to scatterplot', href='/pages/scatterplot1'),
         dcc.Link('Link to ameircan', href='/pages/choropleth2'),
